8d76e7339d5bc9a9d01d4b704f4add826128ed59
Chris Deck <deckchris95@gmail.com>
2024-06-25 17:01:31 -0400

    draft context update

--- app/context/DraftContext.tsx
+++ app/context/DraftContext.tsx
@@ -1,40 +1,77 @@
 "use client";
 
-import { createContext, useContext, useState, ReactNode } from "react";
-import { Team } from "../interfaces/Team"; // Ensure the path is correct
+import {
+  createContext,
+  useContext,
+  useState,
+  useEffect,
+  ReactNode,
+} from "react";
+import { Team } from "../interfaces/Team";
 
 interface DraftContextType {
   teamDraft: Team | null;
-  setTeamDraft: (team: Team | null) => void;
   selectedOpponentDraft: Team | null;
-  setSelectedOpponentDraft: (team: Team | null) => void;
+  setTeamDraft: (team: Team) => void;
+  setSelectedOpponentDraft: (team: Team) => void;
 }
 
 const DraftContext = createContext<DraftContextType | undefined>(undefined);
 
-export const useDraft = () => {
-  const context = useContext(DraftContext);
-  if (!context) {
-    throw new Error("useDraft must be used within a DraftProvider");
-  }
-  return context;
-};
-
 export const DraftProvider = ({ children }: { children: ReactNode }) => {
-  const [teamDraft, setTeamDraft] = useState<Team | null>(null);
-  const [selectedOpponentDraft, setSelectedOpponentDraft] =
+  const [teamDraft, setTeamDraftState] = useState<Team | null>(null);
+  const [selectedOpponentDraft, setSelectedOpponentDraftState] =
     useState<Team | null>(null);
+
+  useEffect(() => {
+    const savedTeamDraft = localStorage.getItem("teamDraft");
+    const savedSelectedOpponentDraft = localStorage.getItem(
+      "selectedOpponentDraft"
+    );
+
+    if (savedTeamDraft) setTeamDraftState(JSON.parse(savedTeamDraft));
+    if (savedSelectedOpponentDraft)
+      setSelectedOpponentDraftState(JSON.parse(savedSelectedOpponentDraft));
+  }, []);
+
+  useEffect(() => {
+    if (teamDraft) localStorage.setItem("teamDraft", JSON.stringify(teamDraft));
+  }, [teamDraft]);
+
+  useEffect(() => {
+    if (selectedOpponentDraft)
+      localStorage.setItem(
+        "selectedOpponentDraft",
+        JSON.stringify(selectedOpponentDraft)
+      );
+  }, [selectedOpponentDraft]);
+
+  const setTeamDraft = (team: Team) => {
+    setTeamDraftState(team);
+  };
+
+  const setSelectedOpponentDraft = (team: Team) => {
+    setSelectedOpponentDraftState(team);
+  };
 
   return (
     <DraftContext.Provider
       value={{
         teamDraft,
-        setTeamDraft,
         selectedOpponentDraft,
+        setTeamDraft,
         setSelectedOpponentDraft,
       }}
     >
       {children}
     </DraftContext.Provider>
   );
+};
+
+export const useDraft = (): DraftContextType => {
+  const context = useContext(DraftContext);
+  if (!context) {
+    throw new Error("useDraft must be used within a DraftProvider");
+  }
+  return context;
 };
