728ec6dbc93acc7efaae8dcd826a402f7deccb14
Claudio Barsante <clbmribas@gmail.com>
2024-10-11 15:55:44 -0300

    --merge Diego's code--

--- app/components/PlayerSearch.tsx
+++ app/components/PlayerSearch.tsx
@@ -18,22 +18,60 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import { toast } from "@/components/ui/use-toast";
-import { Loader2 } from "lucide-react";
+import { Loader2,ChevronDown } from "lucide-react";
 import { useEffect, useState } from "react";
+import {IState, State} from "country-state-city";
 import { Pagination } from "./pagination";
 
+type InputVisibility = {
+  firstName: boolean;
+  lastName: boolean;
+  pdgaNumber: boolean;
+  city: boolean;
+  state: boolean;
+  country: boolean;
+};
+
 function PlayerSearch() {
-  const [name, setName] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [pdgaNumber, setPdgaNumber] = useState("");
+  const [city, setCity] = useState("");
+  const [states, setStates] = useState<IState[]>([]);
+  const [selectedState, setSelectedState] = useState("NJ");
+  const [country] = useState("US");
   const [results, setResults] = useState<any[]>([]);
   const [isLoading, setIsLoading] = useState(false);
 
   const [isMobile, setIsMobile] = useState(false);
+  const [inputVisibility, setInputVisibility] = useState<InputVisibility>(
+    {
+      firstName: true,
+      lastName: true,
+      pdgaNumber: true,
+      city: true,
+      state: true,
+      country: true,
+    }
+  );
   const [paginationConfig, setPaginationConfig] = useState({
     pageIndex: 0,
     perPage: "10",
     totalCount: 0,
   });
+
+  useEffect(() => {
+    const fetchedStates = State.getStatesOfCountry("US");
+    setStates(fetchedStates);
+  }, []);
 
   useEffect(() => {
     const handleResize = () => {
@@ -68,7 +106,7 @@ function PlayerSearch() {
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ firstName, lastName,pdgaNumber, city, state, country}),
+        body: JSON.stringify({ firstName, lastName,pdgaNumber, city, state:selectedState, country}),
       });
       const data = await response.json();
       if (data.error) {
@@ -99,19 +137,133 @@ function PlayerSearch() {
   };
 
   return (
-    <div className="flex flex-1 flex-col p-3 lg:p-4 gap-8">
-      <Card>
-        <CardHeader>
+    <div className="flex flex-1 flex-col p-3 lg:p-0 gap-8 max-w-[1300px]">
+       <Card>
+        <CardHeader className="flex flex-row items-center justify-between">
           <CardTitle>Player Search</CardTitle>
-          {/* <CardDescription>blah blah</CardDescription> */}
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              {isMobile ? (
+                <Button variant="outline" className="pt-0">
+                  ...
+                </Button>
+              ) : (
+                <Button variant="outline" className="ml-auto">
+                  Fields <ChevronDown className="ml-2 h-4 w-4" />
+                </Button>
+              )}
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              {Object.keys(inputVisibility).map((key) => {
+                const typedKey = key as keyof InputVisibility;
+                return (
+                  <DropdownMenuCheckboxItem
+                    key={key}
+                    className="capitalize"
+                    checked={inputVisibility[typedKey]}
+                    onCheckedChange={(value) =>
+                      setInputVisibility((prev) => ({
+                        ...prev,
+                        [typedKey]: value,
+                      }))
+                    }
+                  >
+                    {key}
+                  </DropdownMenuCheckboxItem>
+                );
+              })}
+            </DropdownMenuContent>
+          </DropdownMenu>
         </CardHeader>
         <CardContent>
-          <Input
-            type="text"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            placeholder="Enter player's name"
-          />
+          <div className="flex flex-col md:flex-row gap-4">
+            {inputVisibility.firstName && (
+              <div className="w-full">
+                <Label htmlFor="firstName">First Name</Label>
+                <Input
+                  id="firstName"
+                  type="text"
+                  value={firstName}
+                  onChange={(e) => setFirstName(e.target.value)}
+                  placeholder="Enter first name"
+                />
+              </div>
+            )}
+            {inputVisibility.lastName && (
+              <div className="w-full">
+                <Label htmlFor="lastName">Last Name</Label>
+                <Input
+                  id="lastName"
+                  type="text"
+                  value={lastName}
+                  onChange={(e) => setLastName(e.target.value)}
+                  placeholder="Enter last name"
+                />
+              </div>
+            )}
+            {inputVisibility.pdgaNumber && (
+              <div className="w-full">
+                <Label htmlFor="pdgaNumber">PDGA Number</Label>
+                <Input
+                  id="pdgaNumber"
+                  type="text"
+                  value={pdgaNumber}
+                  onChange={(e) => setPdgaNumber(e.target.value)}
+                  placeholder="Enter PDGA number"
+                />
+              </div>
+            )}
+            {inputVisibility.city && (
+              <div className="w-full">
+                <Label htmlFor="city">City</Label>
+                <Input
+                  id="city"
+                  type="text"
+                  value={city}
+                  onChange={(e) => setCity(e.target.value)}
+                  placeholder="Enter city"
+                />
+              </div>
+            )}
+            {inputVisibility.state && (
+              <div className="w-full">
+                <Label htmlFor="state">State</Label>
+                <DropdownMenu>
+                  <DropdownMenuTrigger asChild>
+                    <Input
+                      id="state"
+                      type="text"
+                      value={selectedState}
+                      placeholder="Select or type state"
+                      onChange={(e) => setSelectedState(e.target.value)}
+                    />
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent className="w-[190px] min-h-[150px] max-h-[300px] overflow-y-auto">
+                    {states.map((s: IState) => (
+                      <DropdownMenuItem
+                        key={s.isoCode}
+                        onClick={() => setSelectedState(s.isoCode)}
+                      >
+                        {s.name}
+                      </DropdownMenuItem>
+                    ))}
+                  </DropdownMenuContent>
+                </DropdownMenu>
+              </div>
+            )}
+            {inputVisibility.country && (
+              <div className="w-full">
+                <Label htmlFor="country">Country</Label>
+                <Input
+                  id="country"
+                  type="text"
+                  value={country}
+                  readOnly
+                  placeholder="US"
+                />
+              </div>
+            )}
+          </div>
         </CardContent>
         <CardFooter className="border-t px-6 py-4">
           <Button onClick={handleSearch}>
